45e1abeeb433838b0ba85a3ddce358c3b478b1d7
yvlnv <ya.volnova@gmail.com>
2020-10-13 13:50:46 +0100

    add a todo app with drag and drop functionality for marking done and deleting

---
+++ main.js
@@ -0,0 +1,94 @@
+// our data
+const state = {
+    todotasks: [],
+    donetasks: []
+}
+
+// instead of => { return our_html }
+const view = (state) => `
+    <header>My to-do list</header>
+    <section class="todosection">
+        <h1>To-do</h1>
+        <ul>
+            ${state.todotasks.map(task => `<li id="${task.id}" draggable="true" ondragstart="app.run('dragFromToDoTask', event)">${task.text}</li>`).join("")}
+        </ul>
+        <div>
+            <form onsubmit="app.run('add', this);return false;">
+                <input name="task" placeholder="add a task" />
+                <button>Add</button>
+            </form>
+        </div>
+    </section>
+    <section class="donesection" ondragover="event.preventDefault()" ondrop="app.run('addToDoneTask', event)">
+        <h1>Done</h1>
+        <ul>
+            ${state.donetasks.map(task => `<li id="${task.id}" draggable="true" ondragstart="app.run('dragFromDoneTask', event)">${task.text}</li>`).join("")}
+        </ul>
+    </section>
+    <section class="deletesection" ondragover="event.preventDefault()" ondrop="app.run('deleteTask', event)">
+        <h1>Delete</h1>
+    </section>
+`
+// all update func return a state
+const update = {
+    add: async (state, form) => {
+        // access data in a form
+        const data = new FormData(form)
+        const task = new task(data.get('task'))
+        const req = new XMLHttpRequest()
+        const tasks = await getch
+        state.todotasks.push(task)
+
+        const tasks = await fetch('/tasks', {
+            method: 'POST',
+            header: {
+
+            },
+            body: JSON.stringify(task)
+
+        }).then(res => res.json())
+        state.todotasks = tasks
+        return state
+    },
+    dragFromToDoTask: (state, event) => {
+        event.dataTransfer.setData('text', event.target.id)
+        return state
+    },
+    addToDoneTask: (state, event) => {
+        const id = event.dataTransfer.getData('text')
+        const index = state.todotasks.findIndex(task => task.id == id)
+        const task = state.todotasks.find(task => task.id == id)
+        state.todotasks.splice(index, 1)
+        state.donetasks.push(task)
+        return state
+    },
+    dragFromDoneTask: (state, event) => {
+        event.dataTransfer.setData('text', event.target.id)
+        return state
+    },
+    deleteTask: (state, event) => {
+        const id = event.dataTransfer.getData('text')
+        const index = state.donetasks.findIndex(task => task.id == id)
+        state.donetasks.splice(index, 1)
+        return state
+    },
+    getTasks: async (state) => {
+        const req = new XMLHttpRequest()
+        req.addEventListener('load', function () {
+            // this.response
+            app.run('addTasks', JSON.parse(this.response))
+        })
+        req.open('GET', '/tasks')
+        req.send()
+        state.todotasks = await fetch('tasks').then(res => res.json())
+        return state
+    },
+    addTasks: (state, tasks) => {
+        // spread operator
+        state.todotasks = [...state.todotasks, ...todotasks]
+        return state
+    }
+}
+
+app.start('todoApp', state, view, update)
+app.run('getTasks')
